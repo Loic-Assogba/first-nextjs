dff7cfa885ed51523e3454d5ad2a2b5de7b1d3e2
Loic-Assogba <loicassogba7@gmail.com>
2026-04-30 14:30:16 +0200

    Add section projects and add framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/section/Header';
+import Projets from './projets/page';
 
 export default function Home() {
     return (
         <main>
             <Header />
+            <Projets />
         </main>
     );
 }
